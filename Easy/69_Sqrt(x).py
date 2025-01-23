acf974683bd32efaa66010eda321aaed6b005b12
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-01-22 18:09:17 -0600

    Solution to LC 69

---
+++ Easy/69_Sqrt(x).py
@@ -0,0 +1,19 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if(x <= 1):
+            return x
+            
+        l, r = 0, x
+
+        while(l <= r):
+            m = l + (r - 1) // 2
+            square = m * m
+
+            if(square == x):
+                return m
+            elif(square < x):
+                l = m + 1
+            else:
+                r = m - 1
+            
+        return r
