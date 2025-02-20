775649f3a3c09c78551ec1b91c8501c78b6d17e0
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-02-20 17:04:04 -0600

    Updated to LC 74

--- Medium/74_Set_Matrix_Zeroes.py
+++ Medium/74_Set_Matrix_Zeroes.py
@@ -5,19 +5,27 @@ class Solution:
         """
         rows = len(matrix)
         cols = len(matrix[0])
-        zeroR = []
-        zeroC = []
+        col0 = 1
 
         for i in range(rows):
             for j in range(cols):
                 if(matrix[i][j] == 0):
-                    zeroR.append(i)
-                    zeroC.append(j)
+                    matrix[i][0] = 0
 
-        for i in zeroR:
+                    if(j != 0):
+                        matrix[0][j] = 0
+                    else:
+                        col0 = 0
+
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if(matrix[i][j] != 0):
+                    if(matrix[i][0] == 0 or matrix[0][j] == 0):
+                        matrix[i][j] = 0
+
+        if(matrix[0][0] == 0):
             for j in range(cols):
-                matrix[i][j] = 0
-        
-        for j in zeroC:
+                matrix[0][j] = 0
+        if(col0 == 0):
             for i in range(rows):
-                matrix[i][j] = 0            
+                matrix[i][0] = 0
