533bdfeb19154ae71cd2179d8f3eb5cc8090e9e8
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-02-26 18:37:06 -0600

    Update to LC 2094

--- Easy/2094_Finding_3_Digit_Even_Numbers.py
+++ Easy/2094_Finding_3_Digit_Even_Numbers.py
@@ -1,15 +1,26 @@
 class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
-        digits.sort()
-        N = len(digits)
-        ans = set()
-        
-        for i in range(N):
-            if(digits[i] != 0):
-                for j in range(N):
-                    if(i != j):
-                        for k in range(N):
-                            if(i != k and j != k and digits[k] % 2 == 0):
-                                ans.add(digits[i] * 100 + digits[j] * 10 + digits[k])
+        hm = {}
+        ans = []
+         
+        for i in digits:
+            hm[i] = hm.get(i, 0) + 1
 
-        return sorted(list(ans))
+        for number in range(100, 999, 2):
+            hm2 = {}
+            x = number
+            while(number > 0):
+                temp = number % 10
+                hm2[temp] = hm2.get(temp, 0) + 1
+                number = number // 10
+            
+            def compare(hm1, hm2):
+                for val in hm2:
+                    if(hm2[val] > hm1.get(val,0)):
+                        return False
+                return True
+
+            if(compare(hm, hm2)):
+                ans.append(x)
+
+        return ans
