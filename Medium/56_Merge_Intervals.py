ac76e5a9bae5b8ed4eab6b12038e158edbd2c163
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-02-26 18:58:31 -0600

    Solution to LC 56

---
+++ Medium/56_Merge_Intervals.py
@@ -0,0 +1,13 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        n = len(intervals)
+        ans = []
+
+        for i in range(n):
+            if(ans == [] or  intervals[i][0] > ans[-1][1]):
+                ans.append(intervals[i])
+            else:
+                ans[-1][1] = max(ans[-1][1], intervals[i][1])
+
+        return ans
