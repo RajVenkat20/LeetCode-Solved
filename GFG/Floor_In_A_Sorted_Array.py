e4527baac8e32fb448dd39c2398fdf547f50a6aa
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-02-28 17:16:16 -0600

    Solution to Floor in a Sorted Array

---
+++ GFG/Floor_In_A_Sorted_Array.py
@@ -0,0 +1,18 @@
+class Solution:
+    #User function Template for python3
+    
+    #Complete this function
+    def findFloor(self,arr,k):
+        low = 0
+        high = len(arr) - 1
+        ans = -1
+        
+        while(low <= high):
+            mid = (low + high) // 2
+            if(arr[mid] <= k):
+                ans = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return ans
