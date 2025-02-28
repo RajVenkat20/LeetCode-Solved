0f330f5b7c641da2b421386760c82bbd36a6e158
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-02-28 17:45:33 -0600

    Update to LC 34

--- Medium/34_Find_First_And_Last_Position_Of_Element_In_Sorted_Array.py
+++ Medium/34_Find_First_And_Last_Position_Of_Element_In_Sorted_Array.py
@@ -1,44 +1,30 @@
 class Solution:
-    def getCeil(self, a, n, x):
-        ceil = -1
-        low = 0
-        high = n - 1
-    
-        while(low <= high):
-            mid = (low + high) // 2
-    
-            if(a[mid] >= x):
-                ceil = mid
-                high = mid - 1
-            else:
-                low = mid + 1
-    
-        return ceil
-
-
-    def getFloor(self, a, n, x):
-        floor = -1
-        low = 0
-        high = n - 1
-    
-        while(low <= high):
-            mid = (low + high) // 2
-    
-            if(a[mid] <= x):
-                floor = mid
-                low = mid + 1
-            else:
-                high = mid - 1
-    
-        return floor
-
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        lb = self.getFloor(nums, n, target)
+        first, last = -1, -1
+        size = len(nums)
 
-        print(lb)
-        
-        if(lb == -1 or nums[lb] != target):
-            return [-1, -1]
+        # Finding the first occurence
+        low, high = 0, size -1
+        while(low <= high):
+            mid = (low + high) // 2
+            if(nums[mid] == target):
+                first = mid
+                high = mid - 1
+            elif(nums[mid] < target):
+                low = mid + 1
+            else:
+                high = mid - 1
 
-        return [min(lb, self.getCeil(nums, n, target)), max(lb, self.getCeil(nums, n, target))]
+        # Finding the first occurence
+        low, high = 0, size - 1
+        while(low <= high):
+            mid = (low + high) // 2
+            if(nums[mid] == target):
+                last = mid
+                low = mid + 1
+            elif(nums[mid] < target):
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return [first, last]
