d65b5d2bddfc3d9939206e72ba6ef9d766ab04fe
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-03-15 13:18:30 -0500

    Solution to LC 215

---
+++ Medium/215_Kth_Largest_Element_In_An_Array.py
@@ -0,0 +1,12 @@
+import heapq
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        minH = nums[:k]
+        heap = heapq.heapify(minH)
+
+        for val in nums[k:]:
+            if(val > minH[0]):
+                heapq.heapreplace(minH, val)
+
+        return heapq.heappop(minH)
