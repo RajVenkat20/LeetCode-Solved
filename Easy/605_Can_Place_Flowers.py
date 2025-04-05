4f669afb7e004f8eb6df3d2d9b8ffa75ddd65442
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-04-05 13:50:13 -0500

    Update to LC 605

--- Easy/605_Can_Place_Flowers.py
+++ Easy/605_Can_Place_Flowers.py
@@ -1,16 +1,11 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         size = len(flowerbed)
-        cnt = 0
-        res = [0]
-        res.extend(flowerbed)
-        res.append(0)
+        res = [0] + flowerbed + [0]
 
         for i in range(1, size + 1):
-            if(res[i - 1] != 1 and res[i + 1] != 1 and res[i] != 1):
+            if(res[i - 1] == 0 and res[i + 1] == 0 and res[i] == 0):
                 res[i] = 1
-                cnt += 1
-                if(cnt == n):
-                    return True
+                n -= 1
 
-        return cnt >= n
+        return n <= 0
