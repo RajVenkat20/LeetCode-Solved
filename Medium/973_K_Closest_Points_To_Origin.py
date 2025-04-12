c2bba587b5fc8ce0ca21f175d76067e4c76fdcff
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-04-12 16:57:55 -0500

    Solution to LC 973

---
+++ Medium/973_K_Closest_Points_To_Origin.py
@@ -0,0 +1,16 @@
+import heapq
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        minHeap = []
+        for x, y in points:
+            minHeap.append([x**2 + y**2, x, y])
+
+        heapq.heapify(minHeap)
+        res = []
+
+        for i in range(k):
+            dist, x, y = heapq.heappop(minHeap)
+            res.append([x, y])
+
+        return res
