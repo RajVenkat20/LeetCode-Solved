cb85add42d7e60f28d47900de6d3eb7bf5be3dfc
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-04-15 16:41:17 -0500

    Update to LC 70

--- Easy/70_Climbing_Stairs.py
+++ Easy/70_Climbing_Stairs.py
@@ -1,15 +1,13 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        if(n == 1 or n == 2):
-            return n
-            
-        a = 1
-        b = 1
-        tot = 0
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp  = [0] * (n + 1)
+        dp[n], dp[n - 1] = 1, 1
 
-        for i in range(2, n + 1):
-            tot = a + b
-            a = b
-            b = tot
-        
-        return tot
+        for i in range(n - 2, -1, -1):
+            dp[i] = dp[i + 1] + dp[i + 2]
+
+        return dp[0]
