e48f76e961ccfc2432c2e1b3ef8a3e1c93328a63
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-05-13 18:38:42 -0500

    Solution to LC 3335

---
+++ Medium/3335_Total_Characters_In_String_After_Transformations_I.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lengthAfterTransformations(self, s: str, t: int) -> int:
+        mod = 10 ** 9 + 7
+        cnt = [0] * 26
+
+        for char in s:
+            cnt[ord(char) - ord('a')] += 1
+
+        for round in range(t):
+            nxt = [0] * 26
+            nxt[0] = cnt[25]
+            nxt[1] = (cnt[25] + cnt[0]) % mod
+            
+            for i in range(2, 26):
+                nxt[i] = cnt[i - 1]
+
+            cnt = nxt
+
+        return sum(cnt) % mod
