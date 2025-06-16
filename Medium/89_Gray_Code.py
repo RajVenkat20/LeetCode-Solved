0bc3135e53169e31dc13b9e530fe7cb9b0b8d0c2
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-06-16 15:48:19 -0500

    Update to LC 89

--- Medium/89_Gray_Code.py
+++ Medium/89_Gray_Code.py
@@ -1,23 +1,6 @@
 class Solution:
     def grayCode(self, n: int) -> List[int]:
-        res, isPresent = [0], set([0])
-
-        def dfs(n):
-            if(len(res) == (1 << n)):
-                return True
-            
-            curr = res[-1]
-
-            for i in range(n):
-                nextNum = curr ^ (1 << i)
-                if(nextNum not in isPresent):
-                    isPresent.add(nextNum)
-                    res.append(nextNum)
-                    if(dfs(n)):
-                        return True
-                    isPresent.remove(nextNum)
-                    res.pop()
-            return False
-
-        dfs(n)
+        res = [0]
+        for i in range(n):
+            res += [x | (1 << i) for x in reversed(res)]
         return res
